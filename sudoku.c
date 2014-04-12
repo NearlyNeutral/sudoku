4aedca9f6741f637c46684e50c63e9b8353a359c
NearlyNeutral <petercsacnj@gmail.com>
2014-04-12 19:34:02 +1200

    Highlighting etc

--- sudoku.c
+++ sudoku.c
@@ -8,31 +8,47 @@
 #define W_WIDTH (8 * W_STREATCH + 3) 
 #define W_HEIGHT (8 * H_STREATCH + 3)
 
+/* Width and heigh of info window */
+#define I_WIDTH 40
+#define I_HEIGHT 2
+
 /* Get coords on window given coords for grid */
 #define wx(G_X) (G_X * W_STREATCH + 1) 
 #define wy(G_Y) (G_Y * H_STREATCH + 1)
 
 
 WINDOW * win; //Main window
+WINDOW * infowin; //Secondary window with commands etc
 int grid[9][9]; //Grid that holds numerical values
 int initgrid[9][9]; //Initial grid, used for saving initial state
 
 /* Cursor coordinates (in terms of grid) */
 int c_y;
 int c_x;
+int highlighted;
 
 int drawwin(); //Draw current window state
 int main_loop(); //Main loop of program
+int cal_progress(); //Calculate how many rows/columns/boxes are complete out of 27
+int draw_infowin();
+int update_complete();
+int highlight(int hnum); //Highlight number
+int special(); //Do special commands
 
 int main () {
 	c_x = c_y = 0;
+	highlighted = 0;
 	initscr();
 	cbreak();
 	noecho();
+//	start_color();
+//	init_pair(1, COLOR_WHITE, COLOR_BLACK);
+//	init_pair(1, COLOR_BLACK, COLOR_WHITE);
 	erase();
 //	printw("Why isn't this working?\n");
 
-	win = newwin(W_HEIGHT, W_WIDTH, 2, 2);
+	win = newwin(W_HEIGHT, W_WIDTH, 1, 1);
+	infowin = newwin(I_HEIGHT, I_WIDTH, 1 + W_HEIGHT, 2);
 	keypad(win, TRUE);
 	int x, y;
 	for (x = 0; x < 9; x++)
@@ -40,8 +56,11 @@ int main () {
 			initgrid[x][y] = 0;
 			grid[x][y] = 0; //Temporary, until grids can be created
 		}
+	initgrid[3][5] = grid[3][5] = 7; //Debug test value
 
 	drawwin();
+	draw_infowin();
+	wmove(win, wy(c_y), wx(c_x));
 //	wmove(win, wy(0), wx(0));
 //	refresh();
 	main_loop();
@@ -65,11 +84,14 @@ int drawwin() {
 	werase(win);
 	box(win, 0, 0);
 	int ix, iy;
+/*	wattron(win, A_BOLD); // May have to be changed if used other than at the immediate start
 	for (iy = 0; iy < 9; iy++)
 		for (ix = 0; ix < 9; ix++) {
 			if (grid[ix][iy]) mvwaddch(win, wy(iy), wx(ix), '0' + grid[ix][iy]);
 			else mvwaddch(win, wy(iy), wx(ix), ' ');
 		}
+	wattroff(win, A_BOLD);*/
+	highlight(0);
 	for (ix = 1; ix < 3; ix++) {
 
 		wmove(win, wy(ix * 3) - H_STREATCH / 2, 1);
@@ -116,6 +138,7 @@ int main_loop() {
 				if (!initgrid[c_x][c_y]) { //Don't overwrite initial grid, only change its zeros
 					waddch(win, ' ');
 					grid[c_x][c_y] = 0;
+					update_complete();
 				}
 				break;
 			case '1':
@@ -130,9 +153,12 @@ int main_loop() {
 				if (!initgrid[c_x][c_y]) {
 					waddch(win, ch);
 					grid[c_x][c_y] = ch - '0';
+					if ((ch - '0') == highlighted) highlight(highlighted); //Highlight new numbers also
+					update_complete();
 				}
 				break;
 			case ':': //Special commands with colon - not implemented
+				special();
 				break;
 			default:
 				break;
@@ -149,4 +175,118 @@ int main_loop() {
 }
 
 
+int cal_progress() {
+	int pr = 0; //counter value
+	int nine[10];
+	int i, i2, hol;
+
+	for (i = 0; i < 9; i++) {
+		//test all nine columns
+		for (i2 = 0; i2 < 10; i2++) //Clear array
+			nine[i2] = 0;
+		for (i2 = 0; i2 < 9; i2++) //Count up frequency of each number
+			nine[grid[i][i2]]++;
+		hol = 0;
+		for (i2 = 1; i2 < 10; i2++) //Count number of places where frequency is 1 (not including 0)
+			if (nine[i2] == 1) 
+				hol++;
+		if (hol == 9) pr++; //If all nine are one, add 1 to pr
+		
+		//test rows in same loop
+		for (i2 = 0; i2 < 10; i2++)
+			nine[i2] = 0;
+		for (i2 = 0; i2 < 9; i2++)
+			nine[grid[i2][i]]++; //Note: Sole difference is order of variables here.
+		hol = 0;
+		for (i2 = 1; i2 < 10; i2++)
+			if (nine[i2] == 1) 
+				hol++;
+		if (hol == 9) pr++;
+	}
+
+	//To do boxes, need two new variables
+	int o, o2; 
+	for (i = 0; i < 9; i += 3)
+		for (o = 0; o < 9; o +=3) //i and o are top-left coords of each box
+		{
+			for (i2 = 0; i2 < 10; i2++)
+				nine[i2] = 0;
+			for (i2 = 0; i2 < 3; i2++)
+				for (o2 = 0; o2 < 3; o2++)
+					nine[grid[i + i2][o + o2]]++;
+			hol = 0;
+			for (i2 = 1; i2 < 10; i2++)
+				if (nine[i2] == 1) 
+					hol++;
+			if (hol == 9) pr++;
+		}
+
+	return pr;
+}
+
+int draw_infowin() {
+	werase(infowin);
+	mvwprintw(infowin, 0, 0, "Complete:   /27   Highlight no.      ");
+	wattron(infowin, A_BOLD);
+	mvwprintw(infowin, 0, 32, ":#");
+	wattroff(infowin, A_BOLD);
+	update_complete();
+//	wrefresh(infowin);
+	return 0;
+}
+
+int update_complete() {
+	wattron(infowin, A_BOLD);
+	mvwprintw(infowin, 0, 10, "%2d", cal_progress());
+	wattroff(infowin, A_BOLD);
+	wrefresh(infowin);
+	return 0;
+}
+
+//Basically redraws the screen - highlight(0) could be used to replace part of drawwin()
+int highlight(int hnum) {
+	int i, i2;
+	for (i = 0; i < 9; i++)
+		for (i2 = 0; i2 < 9; i2++) {
+			wattrset(win, A_NORMAL);
+			if (initgrid[i][i2]) wattron(win, A_BOLD);
+			if ((grid[i][i2] == hnum) && (hnum)) wattron(win, A_REVERSE);
+			if (grid[i][i2]) mvwaddch(win, wy(i2), wx(i), '0' + grid[i][i2]);
+			else mvwaddch(win, wy(i2), wx(i), ' ');
+//			if (grid[i][i2] == hnum) wattroff(win, COLOR_PAIR(1));			
+			
+		}
+
+	wattrset(win, A_NORMAL);
+	wrefresh(win);
+	highlighted = hnum;
+	return 0;
+}
+
+
+int special() {
+	mvwaddch(infowin, 1, 0, ':');
+	wmove(infowin, 1, 1);
+	char c = wgetch(infowin);
+	switch (c) { //Currently only functionality is to highlight characters
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9':		
+			highlight(c - '0');
+			break;
+		default:
+			break;
+	}
+	mvwaddch(infowin, 1, 0, ' ');
+	wrefresh(infowin);	
+
+	return 0;
+}
 
